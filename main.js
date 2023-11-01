1693bc538ed3f86b8f95b0fed285f6423d1855d3
joaopedro043 <129960109+joaopedro043@users.noreply.github.com>
2023-11-01 10:59:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function tocaSom(idElementoAudio){      
+    document.querySelectorAll(idElementoAudio);
+}
+
+    const listaDeTecla = document.querySelectorAll('.tecla');
+
+    let contator = 0;
+   
+    while(contator < 9){
+    const instrumento = listadeteclas[contador];
+        listaDeTecla[contator].onclick = function(){
+    
+    };
+    contator = contator +1;}
