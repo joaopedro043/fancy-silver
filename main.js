fed7d3863803ff3e236de6a0e9b2173e89b93b24
joaopedro043 <129960109+joaopedro043@users.noreply.github.com>
2023-11-08 11:03:04 -0300

    .

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-function tocaSom(idElementoAudio){      
-    document.querySelectorAll(idElementoAudio);
+function tocaSom(seletorAudio){      
+    document.querySelectorAll(seletorAudio).play('')
+     const elemento = listaDeTecla = document.querySelectorAll('.tecla');
+
 }
-
-    const listaDeTecla = document.querySelectorAll('.tecla');
-
-    let contator = 0;
-   
+if(elemento === null){
+   elemento.play();
     while(contator < 9){
     const instrumento = listadeteclas[contador];
         listaDeTecla[contator].onclick = function(){
     
     };
     contator = contator +1;}
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    };
+     tecla.onkeydown = function(){
+if(evento.code === 'Space' || evento.code === 'enter'){
+       
+   
+  
+    }
+tecla.onkeyup = function(){
+tecla.classListe.remove('ativa');
+}
